17caf1d2322092bf8afe282c8320fc094211980e
Thomas Grice <z0d14c@users.noreply.github.com>
2025-08-11 18:41:22 -0400

    Use project description for resume bullets

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,31 +5,26 @@ import circlepopImg from "../assets/projects/circlepop1.png";
 export interface ProjectSummary {
   slug: string;
   title: string;
-  description: string;
   img: ImageMetadata;
   resumeTitle: string;
-  resumeBullet: string;
+  description: string;
 }
 
 export const projectSummaries: ProjectSummary[] = [
   {
     slug: "circlepop",
     title: "Circlepop",
-    description:
-      "Proof of concept for a transit and population density map within a given radius",
     img: circlepopImg,
     resumeTitle: "Circlepop - Transit and Population Within A Circle",
-    resumeBullet:
+    description:
       "Full-stack project using React along with Neon to host PostGRES and Vercel to host the frontend. Reveals population and transit stop data within a given circle.",
   },
   {
     slug: "careernova",
     title: "Careernova",
-    description:
-      "A platform for job seekers to tailor their resume to a given job description",
     img: careernovaImg,
     resumeTitle: "Careernova.pro - Automated Cover Letter Generator",
-    resumeBullet:
+    description:
       "Full-stack project; uses SST framework (as a means to manage AWS infrastructure), React and Typescript. AI and prompt engineering are used to generate cover letters and resume advice for users.",
   },
 ];
