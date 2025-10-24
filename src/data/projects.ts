123c6ec110e045fc91e8141dd7648e882ff4479d
Thomas Grice <augustus.grice@gmail.com>
2025-10-24 15:48:36 -0500

    feat: add boltline inventory metadata project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,6 +3,8 @@ import careernovaImg from "../assets/projects/careernova1.png";
 import circlepopImg from "../assets/projects/circlepop1.png";
 import expressionEditorImg from "../assets/projects/expressioneditor1.png";
 import micasenseImg from "../assets/projects/micasense1.jpeg";
+import stokeImg from "../assets/projects/boltlineinventory.webp";
+
 
 export interface ProjectSummary {
   slug: string;
@@ -15,13 +17,12 @@ export interface ProjectSummary {
 
 export const projectSummaries: ProjectSummary[] = [
   {
-    slug: "circlepop",
-    title: "Circlepop",
-    img: circlepopImg,
-    resumeTitle: "Circlepop - Transit and Population Within A Circle",
-    description:
-      "Full-stack project using React along with Neon to host PostGRES and Vercel to host the frontend. Reveals population and transit stop data within a given circle.",
-    critical: true,
+    slug: "stokeinventory",
+    title: "Stoke/Boltline - Inventory Metadata Rework",
+    img: stokeImg,
+    resumeTitle: "Stoke/Boltline - Inventory Metadata Rework",
+    critical: false, // mostly because I talk about it in the stoke section anyway 
+    description: "Rewrote inventory metadata handling in Stoke's Boltline inventory product to handle lot metadata -- along the way handling concurrency and migration and rollout for millions of pieces of inventory."
   },
   {
     slug: "careernova",
@@ -49,6 +50,15 @@ export const projectSummaries: ProjectSummary[] = [
     description:
       "Ported the existing Backbonejs-based app to React, using React + Leaflet to display multispectral data",
     critical: false,
+  },
+  {
+    slug: "circlepop",
+    title: "Circlepop",
+    img: circlepopImg,
+    resumeTitle: "Circlepop - Transit and Population Within A Circle",
+    description:
+      "Full-stack project using React along with Neon to host PostGRES and Vercel to host the frontend. Reveals population and transit stop data within a given circle.",
+    critical: true,
   },
 ];
 
